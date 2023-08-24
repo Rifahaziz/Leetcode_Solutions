f18ca6fe46063e2665f2fbf0ab9ec0abc2da4786
Rifah Sama Aziz <42877341+Rifahaziz@users.noreply.github.com>
2023-08-24 12:44:07 -0400

    Create find_the_Index_of_the_First_Occurrence_in_a_String.py

---
+++ find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        return haystack.find(needle)
+
+#Runtime: 14 ms
+#Memory Usage: 13.5 MB
